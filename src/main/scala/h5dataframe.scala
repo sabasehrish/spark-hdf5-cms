02a4a6005bff0e208bad87d76d6341ea536f8a58
Saba Sehrish <ssehrish@fnal.gov>
2016-09-28 22:11:48 -0700

    Moved DataFrame creation to a new file

---
+++ src/main/scala/h5dataframe.scala
@@ -0,0 +1,40 @@
+import Filters._
+import H5Read._
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd._
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions._
+
+object H5DataFrame {
+
+/*Creating Spark DataFrame from HDF5 Datasets */
+  def createDataFrame(spark: SparkSession, rdd: RDD[Row], gname: String, dsnames: List[String]) : DataFrame = {
+    import spark.implicits._
+    val colnames = generateColnames(dsnames)
+    val df = gname match {
+                          case "/Muon/" => rdd.map(x => MuonRow(x.getFloat(0), x.getFloat(1), x.getFloat(2), x.getInt(3), x.getInt(4), x.getInt(5), x.getFloat(6), x.getFloat(7), x.getFloat(8), x.getInt(9), x.getFloat(10))).toDF(colnames: _*)
+                          case "/Tau/"  => rdd.map(x => TauRow(x.getFloat(0), x.getFloat(1), x.getFloat(2), x.getInt(3), x.getInt(4), x.getInt(5), x.getLong(6), x.getFloat(7))).toDF(colnames: _*)
+                          case "/Photon/"  => rdd.map(x => PhotonRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getFloat(3), x.getFloat(4), x.getFloat(5), x.getFloat(6), x.getFloat(7), x.getFloat(8), x.getFloat(9), x.getFloat(10), x.getFloat(11))).toDF(colnames: _*)
+                          case "/Electron/"  => rdd.map(x => ElectronRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getFloat(3), x.getFloat(4), x.getFloat(5), x.getFloat(6), x.getFloat(7), x.getFloat(8), x.getFloat(9), x.getFloat(10), x.getFloat(11), x.getFloat(12), x.getFloat(13), x.getFloat(14), x.getFloat(15), x.getFloat(16), x.getFloat(17), x.getInt(18), x.getShort(19))).toDF(colnames: _*)
+                          case "/AK4Puppi/"  => rdd.map(x => JetRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getFloat(3), x.getFloat(4), x.getFloat(5), x.getFloat(6), x.getFloat(7), x.getFloat(8), x.getFloat(9), x.getInt(10), x.getInt(11))).toDF(colnames: _*)
+                          case "/CA15Puppi/"  => rdd.map(x => VJetRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getFloat(3), x.getFloat(4), x.getFloat(5), x.getFloat(6), x.getFloat(7), x.getFloat(8), x.getFloat(9), x.getInt(10), x.getInt(11))).toDF(colnames: _*)
+                          case "/Info/" => rdd.map(x => InfoRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getInt(3), x.getFloat(4), x.getFloat(5), x.getFloat(6), x.getFloat(7))).toDF(colnames: _*)
+                          case "/GenEvtInfo/" => rdd.map(x => GenInfoRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getFloat(3), x.getFloat(4))).toDF(colnames: _*)
+    }
+    return df
+  }
+
+  case class MuonRow(eta: Float, pt: Float, phi: Float, evtNum: Int, runNum: Int, lumisec:Int, chHadIso: Float, neuHadIso: Float, puIso: Float, pogIDBits: Int, gammaIso: Float)
+  case class TauRow(eta: Float, pt: Float, phi: Float, evtNum: Int, runNum: Int, lumisec:Int, hpsDisc: Long, rawIso3Hits: Float)
+  case class InfoRow(runNum: Int, lumiSec: Int, evtNum: Int, metFilterFailBits: Int, pfMET: Float, pfMETphi: Float, puppET: Float, puppETphi: Float)
+  case class GenInfoRow(runNum: Int, lumisec: Int, evtNum: Int, weight: Float, scalePDF:Float)
+  case class PhotonRow(runNum: Int, lumisec: Int, evtNum: Int, eta: Float, pt: Float, phi: Float, chHadIso: Float, scEta: Float, neuHadIso: Float, gammaIso: Float, sieie: Float, sthovere: Float)
+  // 19 datasets from Electron Branch!!!
+  case class ElectronRow(runNum: Int, lumisec: Int, evtNum: Int, eta: Float, pt: Float, phi: Float, chHadIso: Float, neuHadIso: Float, gammaIso: Float, scEta: Float, sieie: Float, hovere: Float, eoverp: Float, dEtaIn: Float, dPhiIn: Float, ecalEnergy: Float, d0: Float, dz: Float, nMissingHits: Int, isConv: Short)
+  case class JetRow(runNum: Int, lumisec: Int, evtNum: Int, eta: Float, pt: Float, phi: Float, chHadFrac: Float, chEmFrac: Float, neuHadFrac: Float, neuEmFrac: Float, nParticles: Int, nCharged: Int)
+  case class VJetRow(runNum: Int, lumisec: Int, evtNum: Int, eta: Float, pt: Float, phi: Float, chHadFrac: Float, chEmFrac: Float, neuHadFrac: Float, neuEmFrac: Float, nParticles: Int, nCharged: Int)
+
+ }
--- src/main/scala/skimnslim.scala
+++ src/main/scala/skimnslim.scala
@@ -20,34 +20,9 @@ import ncsa.hdf.hdf5lib.H5
 import ncsa.hdf.hdf5lib.HDF5Constants
 import Filters._
 import H5Read._
+import H5DataFrame._
 
 object skimslim {
-
-  /*Creating Spark DataFrame from HDF5 Datasets */
-  def createDataFrame(rdd: RDD[Row], spark: SparkSession, dname: String, gname: String, dsnames: List[String]) : DataFrame = {
-    val colnames = generateColnames(dsnames)
-    //val files = getListOfFiles(dname)
-   // val partitionlist = getPartitionInfo(dname, gname+dsnames(0))
-    import spark.implicits._
-  //  val rdd = sc.parallelize(partitionlist, partitionlist.length).flatMap(x => readDatasets(dname+x.fname, gname, dsnames, x.begin, x.end))
-    var df = gname match {
-                          case "/Muon/" => rdd.map(x => MuonRow(x.getFloat(0), x.getFloat(1), x.getFloat(2), x.getInt(3), x.getInt(4), x.getInt(5), x.getFloat(6), x.getFloat(7), x.getFloat(8), x.getInt(9)))
-                                              .toDF(colnames: _*)
-                          case "/Tau/"  => rdd.map(x => TauRow(x.getFloat(0), x.getFloat(1), x.getFloat(2), x.getInt(3), x.getInt(4), x.getInt(5), x.getLong(6), x.getFloat(7)))
-                                              .toDF(colnames: _*)
-                          case "/Photon/"  => rdd.map(x => PhotonRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getFloat(3), x.getFloat(4), x.getFloat(5), x.getFloat(6), x.getFloat(7), x.getFloat(8), x.getFloat(9), x.getFloat(10), x.getFloat(11))).toDF(colnames: _*)
-                          case "/Info/" => rdd.map(x => InfoRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getInt(3), x.getFloat(4), x.getFloat(5), x.getFloat(6), x.getFloat(7))).toDF(colnames: _*)
-                          case "/GenEvtInfo/" => rdd.map(x => GenInfoRow(x.getInt(0), x.getInt(1), x.getInt(2), x.getFloat(3), x.getFloat(4))).toDF(colnames: _*)
-
-    }
-    return df
-  }
-
-  case class MuonRow(eta: Float, pt: Float, phi: Float, evtNum: Int, runNum: Int, lumisec:Int, chHadIso: Float, neuHadIso: Float, puIso: Float, pogIDBits: Int)
-  case class TauRow(eta: Float, pt: Float, phi: Float, evtNum: Int, runNum: Int, lumisec:Int, hpsDisc: Long, rawIso3Hits: Float)
-  case class InfoRow(runNum: Int, lumiSec: Int, evtNum: Int, metFilterFailBits: Int, pfMET: Float, pfMETphi: Float, puppET: Float, puppETphi: Float)
-  case class GenInfoRow(runNum: Int, lumisec: Int, evtNum: Int, weight: Float, scalePDF:Float)
-  case class PhotonRow(runNum: Int, lumisec: Int, evtNum: Int, eta: Float, pt: Float, phi: Float, chHadIso: Float, scEta: Float, neuHadIso: Float, gammaIso: Float, sieie: Float, sthovere: Float)
 
   def main(args: Array[String]) {
     val sc = new SparkContext()
@@ -63,7 +38,7 @@ object skimslim {
     val gname = "/GenEvtInfo/"
     val partitionlist = getPartitionInfo(dname, gname+genevtinfo_ds(0))
     val rdd = sc.parallelize(partitionlist, partitionlist.length).flatMap(x => readDatasets(dname+x.fname, gname, genevtinfo_ds, x.begin, x.end))
-    val genevtinfo_df = createDataFrame(rdd, spark, dname, "/GenEvtInfo/", genevtinfo_ds)
+    val genevtinfo_df = createDataFrame(spark, rdd, "/GenEvtInfo/", genevtinfo_ds)
     genevtinfo_df.cache()
     println("Num events: " + genevtinfo_df.count())
     var t0 = System.nanoTime()
@@ -77,7 +52,7 @@ object skimslim {
     val tau_ds: List[String] = List("Tau.eta", "Tau.pt", "Tau.phi", "Tau.evtNum", "Tau.runNum", "Tau.lumisec", "Tau.hpsDisc", "Tau.rawIso3Hits")
     val tau_pl = getPartitionInfo(dname, tau_gn+"Tau.eta")
     val tau_rdd = sc.parallelize(tau_pl, tau_pl.length).flatMap(x=> readDatasets(dname+x.fname, tau_gn, tau_ds, x.begin, x.end))
-    val tau_df = createDataFrame(tau_rdd, spark, dname, tau_gn, tau_ds)
+    val tau_df = createDataFrame(spark, tau_rdd, tau_gn, tau_ds)
     tau_df.show()
     val tau_fdf = filterTauDF(spark, tau_df)
     val tdf = tau_fdf.groupBy("Tau_evtNum", "Tau_lumisec", "Tau_runNum").max("Tau_pt")
@@ -88,7 +63,7 @@ object skimslim {
     val pho_ds: List[String] = List("Photon.runNum", "Photon.lumisec", "Photon.evtNum", "Photon.eta", "Photon.pt", "Photon.phi", "Photon.chHadIso", "Photon.scEta", "Photon.neuHadIso", "Photon.gammaIso", "Photon.sieie", "Photon.sthovere")
     val pho_pl = getPartitionInfo(dname, pho_gn+"Photon.eta")
     val pho_rdd = sc.parallelize(pho_pl, pho_pl.length).flatMap(x=> readDatasets(dname+x.fname, pho_gn, pho_ds, x.begin, x.end))
-    val pho_df = createDataFrame(pho_rdd, spark, dname, pho_gn, pho_ds)
+    val pho_df = createDataFrame(spark, pho_rdd, pho_gn, pho_ds)
     pho_df.show()
 
     }
