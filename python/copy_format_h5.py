dab99795613889ffb009ec15a8f922ef478c8ee1
Jim Kowalkowski <jbk@fnal.gov>
2016-10-15 22:34:24 -0500

    half of the changes

--- python/copy_format_h5.py
+++ python/copy_format_h5.py
@@ -1,26 +1,42 @@
 
 import h5py
+import sys
+import os
+import glob
 
-in_dir = '/scratch/ssehrish/h5out/TTJets_13TeV_amcatnloFXFX_pythia8_2'
-out_dir = '/scratch2/jbk'
+# in_dir = '/scratch/ssehrish/h5out/TTJets_13TeV_amcatnloFXFX_pythia8_2'
+# out_dir = '/scratch2/jbk'
 
-fname_out = out_dir + '/' + 'TTJets_13TeV_amcatnloFXFX_pythia8_2_all.h5'
-fname_in = in_dir + '/' + 'TTJets_13TeV_amcatnloFXFX_pythia8_2_10.h5'
+# fname_out = out_dir + '/' + 'TTJets_13TeV_amcatnloFXFX_pythia8_2_all.h5'
+# fname_in = in_dir + '/' + 'TTJets_13TeV_amcatnloFXFX_pythia8_2_10.h5'
 
-print fname_out
-print fname_in
+# print fname_out
+# print fname_in
 
-f_in = h5py.File(fname_in,'r')
-f_out = h5py.File(fname_out,'a')
+def copy_format(f_in, f_out):
+	for g in f_in.keys():
+		print g
+		grp = f_out.create_group(g)
+        	# create datasets for sample type and index
+		grp.create_dataset('/'+g+'/'+"stype", shape=(0,), 
+				   dtype=np.int16,maxshape=(None,))
+		grp.create_dataset('/'+g+'/'+"snum", shape=(0,), 
+				   dtype=np.int16,maxshape=(None,))
+		for d in f_in['/'+g].keys():
+			name = '/'+g+'/'+d
+			src = f_in[name]
+			dset = grp.create_dataset(d, shape=(0,), 
+						  dtype=src.dtype,maxshape=(None,))
+			print name, dset.size, dset.shape
+			
+if __name__ == "__main__":
 
-for g in f_in.keys():
-	print g
-	grp = f_out.create_group(g)
-	for d in f_in['/'+g].keys():
-		name = '/'+g+'/'+d
-		src = f_in[name]
-		dset = grp.create_dataset(d, shape=(0,), dtype=src.dtype,maxshape=(None,))
-		print name, dset.size, dset.shape
-
-f_in.close()
-f_out.close()
+	if len(sys.argv)<2:
+		print "bad args.  ", sys.argv[0], "dir_where_files_are"
+		sys.exit(-1)
+		
+	f_in = h5py.File(fname_in,'r')
+	f_out = h5py.File(fname_out,'a')
+	copy_format(f_in, f_out)
+	f_in.close()
+	f_out.close()
--- python/merge_h5.py
+++ python/merge_h5.py
@@ -1,30 +1,41 @@
 
 import h5py
 import sys
+import binascii
+import os
+import glob
+import copy_format_h5.copy_format as copy_format
 
-in_dir = '/scratch/ssehrish/h5out/TTJets_13TeV_amcatnloFXFX_pythia8_2'
-out_dir = '/scratch2/jbk'
+# adds one file at a time to a file that will contain everything
 
-if len(sys.argv)<2: 
-	print "wrong arguments, give in and out (in that order"
-	sys.exit(-1)
-
-print sys.argv[1], sys.argv[2]
+# in_dir = '/scratch/ssehrish/h5out/TTJets_13TeV_amcatnloFXFX_pythia8_2'
+# out_dir = '/scratch2/jbk'
 
 # fname_out = out_dir + '/' + 'TTJets_13TeV_amcatnloFXFX_pythia8_2_all.h5'
 # fname_in = in_dir + '/' + 'TTJets_13TeV_amcatnloFXFX_pythia8_2_0.h5'
 
-fname_out = out_dir + '/' + sys.argv[2]
-fname_in = in_dir + '/' + sys.argv[1]
-
-print fname_out
-print fname_in
-
 f_in = h5py.File(fname_in,'r')
 f_out = h5py.File(fname_out,'a')
 
+# binascii.crc32 
+tmp_dir = os.path.basename(os.path.dirname(fname_in))
+tmp_hash = binascii.crc32(tmp_dir)
+tmp_type = binascii.crc32(tmp.dir.split("_",1)[0])
+
 for g in f_in.keys():
 	print g
+	name = '/'+g+'/'
+        ssize = f_in[name+'runNum'].size
+        stype_out = f_out[name+'stype']
+        snum_out = f_out[name+'snum']
+        old_size=stype_out.size
+        new_size=stype_out+ssize
+        stype_out.resize( (new_size,) )
+        snum_out.resize( (new_size,) )
+        stype_out[old_size:new_size] = tmp_type
+        snum_out[old_size:new_size] = tmp_hash
+        # stype_out[]
+        # still need to generate the values within this group
 	for d in f_in['/'+g].keys():
 		name = '/'+g+'/'+d
 		dset_src = f_in[name]
@@ -38,3 +49,15 @@ for g in f_in.keys():
 
 f_in.close()
 f_out.close()
+
+if __name__ == '__main__':
+	if len(sys.argv)<2: 
+		print "wrong arguments, give in and out (in that order)"
+		sys.exit(-1)
+
+	fname_out = out_dir + '/' + sys.argv[2]
+	fname_in = in_dir + '/' + sys.argv[1]
+
+	print fname_out
+	print fname_in
+
